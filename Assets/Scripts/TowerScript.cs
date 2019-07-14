fa3905fdf05edca1c9021dd0e1234434fa46d1a5
wardwaren <wardwaren@gmail.com>
2019-07-14 15:04:33 +0600

    Added ability to return towers to hand.

--- Assets/Scripts/TowerScript.cs
+++ Assets/Scripts/TowerScript.cs
@@ -22,6 +22,7 @@ public class TowerScript : MonoBehaviour
 
     GameObject TowerInfo;
     TowerPanel TowerPanel;
+    GameObject TowerOfferPanel;
     Level LevelScript;
     float HoldTimer = 0f;
 
@@ -30,15 +31,19 @@ public class TowerScript : MonoBehaviour
     bool dragging = false;
     Color TowerColor;
     Color OldColor;
+    Vector3 oldPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         TowerInfo = GameObject.FindWithTag("TowerInfo");
         Level = GameObject.FindWithTag("Level");
+        TowerOfferPanel = GameObject.FindWithTag("TowerOfferPanel");
         TowerPanel = TowerInfo.GetComponent<TowerPanel>();
         LevelScript = Level.GetComponent<Level>();
         TowerColor = gameObject.GetComponent<Renderer>().material.color;
         OldColor = TowerColor;
+        oldPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -67,8 +72,9 @@ public class TowerScript : MonoBehaviour
         if(HoldTimer >= 1)
         {
             dragging = true;
-            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, LevelScript.getZfield() + 15f);
+            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, LevelScript.getZfield() + 14.5f);
             Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+
             transform.position = cursorPosition;
             TowerColor.r = 0.5f;
             gameObject.GetComponent<Renderer>().material.color = TowerColor;
@@ -81,7 +87,6 @@ public class TowerScript : MonoBehaviour
         if(dragging == true)
         {
             RaycastHit objectHit;
-            Destroy(gameObject);
             Vector3 fwd = gameObject.transform.TransformDirection(Vector3.forward);
             Debug.DrawRay(gameObject.transform.position, fwd * 50, Color.green);
             if (Physics.Raycast(gameObject.transform.position, fwd, out objectHit, 50))
@@ -89,13 +94,40 @@ public class TowerScript : MonoBehaviour
                 Vector3 wordPos = objectHit.point;
                 if (objectHit.transform.tag == "PlayField")
                 {
-                    Instantiate(gameObject, wordPos, transform.rotation);
+                    transform.position = wordPos;
+                   // Instantiate(gameObject, wordPos, transform.rotation);
                     LevelScript.SetTower(-1);
                     gameObject.GetComponent<Renderer>().material.color = OldColor;
+                    oldPosition = transform.position;
+                }
+                else if(objectHit.transform.tag == "Panel")
+                {
+                    for (int i = 0; i < TowerOfferPanel.transform.childCount; i++)
+                    {
+                        GameObject child = TowerOfferPanel.transform.GetChild(i).gameObject;
+                        if (child.activeSelf == false)
+                        {
+                            GameObject Tower = Level.GetComponent<Level>().GetTowerByNum(id + 1);
+                            Level.GetComponent<Level>().TowerBought(id + 1);
+                            Button button = child.GetComponent<Button>();
+                            button.GetComponentInChildren<Text>().text = (id + 1).ToString();
+                            button.GetComponent<ButtonScript>().setTowerNumber(id + 1);
+                            child.SetActive(true);
+                            Destroy(gameObject);
+                            break;
+                        }
+                        if(i == TowerPanel.transform.childCount - 1)
+                        {
+                            transform.position = oldPosition;
+                            gameObject.GetComponent<Renderer>().material.color = OldColor;
+                        }
+
+                    }
                 }
                 else
                 {
-                   Debug.Log("Not PlayField");
+                    transform.position = oldPosition;
+                    gameObject.GetComponent<Renderer>().material.color = OldColor;
                 }
 
             }
